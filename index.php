bca3a16f1f5425c3805ef642e3493fc1c063c2aa
Francis <fransterx123@gmail.com>
2017-04-15 12:48:32 -0400

    First commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<!--
+  Author: Francis Keith Mangornong
+
+  HTML for Project Part 2 CS 1520
+-->
+
+<?php require_once(email.php) ?>
+
+
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <meta charset="utf-8">
+    <title>Francis Mangornong</title>
+    <!-- boostrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/styles.css">
+  </head>
+
+  <body>
+    <header>
+      <h1>Francis Mangornong</h1>
+    </header>
+
+    <main>
+
+      <div class="description">
+        <section>
+          <p>Click on the links below to find out more or contact me.</p>
+        </section>
+      </div>
+
+      <div class= "line"></div>
+
+      <br>
+
+      <div class="links">
+        <section>
+          <nav>
+            <ul>
+              <li><a href="about.html">About Me</a></li>
+              <li><a href="contact.html">Contact Me</a></li>
+            </ul>
+          </nav>
+        </section>
+      </div>
+
+    </main>
+
+    <footer>
+      <!-- To do -->
+    </footer>
+
+    <script type="text/javascript" src ="./js/script.js"></script>
+
+  </body>
+
+</html>
